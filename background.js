779306328c5f83a60f9373a1ccde344da9e446a3
Jesse Renee Beach <splendidnoise@gmail.com>
2015-10-07 10:59:28 -0700

    Responding to code review for background.js

--- background.js
+++ background.js
@@ -1,21 +1,37 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+  const COMPLETE = 'complete';
+
   chrome.automation.getTree(function(rootNode) {
     if (!rootNode) {
       return;
     }
 
-    chrome.tabs.onUpdated.addListener(function(tabId, processInfo, tabInfo) {
-      if (processInfo.status === 'complete') {
-        var win = chrome.extension.getViews({
-          type: 'tab',
-          windowId: tabInfo.windowId
-        })[0];
-        if (win && win.createTree) {
-          win.createTree(rootNode);
-        }
+    let processTree = function (tabInfo) {
+      let win = chrome.extension.getViews({
+        type: 'tab',
+        windowId: tabInfo.windowId
+      })[0];
+      if (win && win.createTree) {
+        win.createTree(rootNode);
+      }
+    }
+
+    let onUpdatedHandler = function(tabId, processInfo, tabInfo) {
+      if (processInfo.status === COMPLETE) {
+        processTree(tabInfo);
+        chrome.tabs.onUpdated.removeListener(onUpdatedHandler);
+      }
+    };
+
+    chrome.windows.create({
+      url: chrome.extension.getURL("tree.html" )
+    }, function(winInfo) {
+      let tabInfo = winInfo.tabs[0];
+      if (tabInfo && tabInfo.status === COMPLETE) {
+        processTree(tabInfo);
+      } else {
+        chrome.tabs.onUpdated.addListener(onUpdatedHandler);
       }
     });
-
-    chrome.windows.create({ url: chrome.extension.getURL("tree.html" )});
   });
 });
